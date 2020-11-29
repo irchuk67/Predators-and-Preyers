de180416bdd2934c0d251fbf82a71cfd58780a56
irchuk67 <foxmaster110518@gmail.com>
2020-11-29 16:08:45 +0200

    Correct mistakes in previous commit.2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,8 @@
-import cells.Food;
-import cells.Pray;
 import managers.Controller;
 
 public class Main {
     public static void main(String[] args) {
-        //Controller controller = new Controller();
-        //controller.startSimulation();
-        Food food = new Food(6);
-        Pray pray = new Pray(10, 3);
-        food.setCoordinates(5,9);
-        pray.setCoordinates(13, 9);
-        while (food.getI() != pray.getI() || food.getJ() != pray.getJ()){
-            System.out.print(pray.getI() + ",");
-            System.out.println(pray.getJ());
-            pray.move(food);
-        }
-        System.out.print(pray.getI() + ",");
-        System.out.println(pray.getJ());
+        Controller controller = new Controller();
+        controller.startSimulation();
     }
 }
