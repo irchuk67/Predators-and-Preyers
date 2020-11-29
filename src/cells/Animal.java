39e6614ba9a3501245344bd05c55b077da46ec78
irchuk67 <foxmaster110518@gmail.com>
2020-11-29 16:02:14 +0200

    Correct mistakes in previous commit

--- src/cells/Animal.java
+++ src/cells/Animal.java
@@ -1,34 +1,56 @@
 package cells;
 
-public abstract class Cell {
-    protected int i;
-    protected int j;
-    protected int liveDuration;
+import visualComponents.Map;
 
-    public Cell(int liveDuration) {
-        this.liveDuration = liveDuration;
+public abstract class Animal extends Cell {
+    protected int reproductionCounter;
+    protected boolean isTracking;
+    protected Cell aim;
+
+    public Animal(int liveDuration, int reproductionCounter) {
+        super(liveDuration);
+        this.reproductionCounter = reproductionCounter;
     }
 
-    public int getLiveDuration() {
-        return liveDuration;
+    protected int countDistance(int i, int j) {
+        return Math.abs(this.i - i) + Math.abs(this.j - j);
     }
 
-    public void setLiveDuration(int liveDuration) {
-        this.liveDuration = liveDuration;
+    public abstract Cell searchFood(Map map);
+
+    public abstract void eat();
+
+    public abstract void reproduction();
+
+    public void move(Cell cell) {  //TODO: Exceptions in the next lab
+        if (i < cell.getI()) {
+            i++;
+        } else if (i > cell.getI()) {
+            i--;
+        } else if (j < cell.getJ()) {
+            j++;
+        } else if (j > cell.getJ()) {
+            j--;
+        }
     }
 
-    public int getI() {
-        return i;
+    public int getReproductionCounter() {
+        return reproductionCounter;
     }
 
-    public void setCoordinates(int i, int j) {
-        this.i = i;
-        this.j = j;
+    public boolean isTracking() {
+        return isTracking;
     }
 
-    public int getJ() {
-        return j;
+    public void setTracking(boolean tracking) {
+        isTracking = tracking;
     }
 
-    public abstract Cell clone();
+    public Cell getAim() {
+        return aim;
+    }
+
+    public void setAim(Cell aim) {
+        this.aim = aim;
+    }
 }
